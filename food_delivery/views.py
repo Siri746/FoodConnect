1d09be1de5a2dead52ba42ae99104b0d5da9e26c
Sirivallika Bondalapati <158497611+Siri746@users.noreply.github.com>
2025-03-22 15:02:24 +0900

    Update views.py

--- food_delivery/views.py
+++ food_delivery/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
-from .forms import SignUpForm
-from .models import Profile, FoodRequest, SellerItem
+from food_delivery.forms import SignUpForm  # Absolute import
+from food_delivery.models import Profile, FoodRequest, SellerItem
 
 def landing_page(request):
     if request.method == 'POST':
